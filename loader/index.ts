d7fa03fdf767a5daffc09a1ba20af2199b257196
foul11 <admin@arturka.net>
2025-06-13 09:05:34 +0700

    fix loader

--- loader/index.ts
+++ loader/index.ts
@@ -96,7 +96,7 @@ function Loader(this: LoaderContext<LoaderOptions>, sourceCode: string, inputSou
     const callback = this.async()
 
     // Obfuscates commented source code
-    const options = loaderUtils.getOptions<WebpackObfuscatorOptions>({ query: context.query.toString() }) || {};
+    const options = loaderUtils.getOptions<WebpackObfuscatorOptions>(context as { query: string }) || {};
     const { obfuscatedSource, obfuscationSourceMap } = JavaScriptObfuscator.obfuscate(
         sourceCode,
         {
